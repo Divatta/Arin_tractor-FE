42f865be51568241d6fa1aab59880d644f967349
Dasari Bhargavi <dasaribhargavi35@gmail.com>
2023-09-23 17:47:36 +0530

    Content added in service pages

--- src/Pages/DoorToDoor.jsx
+++ src/Pages/DoorToDoor.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ServicesPageContent from '../Components/ServicesPagesContent';
+import CommonBanner from '../Components/CommonBanner';
 
 function DoorToDoor() {
     const images = [
@@ -10,14 +11,14 @@ function DoorToDoor() {
        
       ];
     
-      const content = `If you are looking for the best door-to-door service solutions in the USA, then ArinTractorwala is best for you. Our experts know that in a globalized today world, businesses depend heavily on reliable and efficient transportation solutions and ensure the smooth flow of goods across global borders. Our door-to-door services are designed best to meet your needs. Thus, we provide you with complete and hassle-free services that fit your import and export needs. Our door-to-door services comprise a diverse range of logistics solutions that meet your needs. Whether you want to export your goods to international markets or import goods, we ensure to move your cargo efficiently and reach to right destination on time without any hassles. Our customization door-to-door solutions ensure that every client meets their specific needs because we understand every client has unique requirements and work accordingly. 
-      While providing door-to-door solutions to our clients, we take some important factors into account, like budget, delivery time, and nature of your cargo. From small size goods to large quantity of goods, we have experience to handle all. The good thing is that door-to-door services offered by us cover your entire logistics chain, from the time when cargo is carried from the source till it reaches to exact destination. By getting door-to-door services from us, you can easily navigate from complex world of international trade. Moreover, our experts are also well-versed in import and export paperwork and ensure that all important documentation is in order. We ensure that your every shipment meets the industry requirements and reduces the risk of delays or any damages. In short, the ArinTractorwala door-to-door services reduce the risks effectively. We always prioritize the safety and security of your goods throughout the whole journey.`;
-       
+      const content = `If you are looking for the best door-to-door service solutions in the USA, then ArinTractorwala is best for you. Our experts know that in a globalized today world, businesses depend heavily on reliable and efficient transportation solutions and ensure the smooth flow of goods across global borders. Our door-to-door services are designed best to meet your needs. Thus, we provide you with complete and hassle-free services that fit your import and export needs. Our door-to-door services comprise a diverse range of logistics solutions that meet your needs. Whether you want to export your goods to international markets or import goods, we ensure to move your cargo efficiently and reach to right destination on time without any hassles. Our customization door-to-door solutions ensure that every client meets their specific needs because we understand every client has unique requirements and work accordingly. `
+      const content2 = `While providing door-to-door solutions to our clients, we take some important factors into account, like budget, delivery time, and nature of your cargo. From small size goods to large quantity of goods, we have experience to handle all. The good thing is that door-to-door services offered by us cover your entire logistics chain, from the time when cargo is carried from the source till it reaches to exact destination. By getting door-to-door services from us, you can easily navigate from complex world of international trade. Moreover, our experts are also well-versed in import and export paperwork and ensure that all important documentation is in order. We ensure that your every shipment meets the industry requirements and reduces the risk of delays or any damages. In short, the ArinTractorwala door-to-door services reduce the risks effectively. We always prioritize the safety and security of your goods throughout the whole journey.`;
     
       return (
         <div>
-          <h1 style={{textAlign: "center"}}>Door to Door Page</h1>
-          <ServicesPageContent images={images} content={content} />
+          <CommonBanner title="Door To Door"/>
+          {/* <h1 style={{textAlign: "center"}}>Door to Door Page</h1> */}
+          <ServicesPageContent images={images} content={content} content2={content2}/>
         </div>
       );
 }
--- src/Pages/Shipping.jsx
+++ src/Pages/Shipping.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ServicesPageContent from '../Components/ServicesPagesContent';
+import CommonBanner from '../Components/CommonBanner';
 
 function Shipping() {
     const images = [
@@ -10,15 +11,15 @@ function Shipping() {
        
       ];
     
-      const content = `Arin Tractorwala is your best-trusted partner if you are looking for shipping services in the USA. We take pride in giving you top-notch shipping solutions that meet your export and import needs. With our cutting-edge technology, commitment to excellence, and diverse network of partners, we ensure that your goods reach to right destination efficiently and safely. We provide a wide range of shipping services that meet your needs. Whether you want to export goods, import goods, or want some logistics solutions, then ArinTractorwala is the perfect solution for you.
-                    Our ocean freight shipping services are designed well to transit a large number of goods across seas. We make strong relationships with shipping lines that allow us to give timely deliveries and competitive rates to our clients. When time is your priority, our air freight services give you an efficient and fast shipping solution. We have a relationship with leading airlines that ensure that your cargo is reached safely to its destination on time. Our team handles all steps of the process, from packaging and customs clearance to timely delivery. We navigate the complexity of your custom regulations and overcome challenges. Our experienced team makes the process simpler, ensures that our compliance is met with customer requirements, and reduces delays. 
-                      We provide you the state of state-of-the-art warehousing facilities and optimize your supply chain. Our shipping services are designed well to meet your demands and ensure that your products reach your destination on time. For heavy or oversize cargo, we are experts in handling project logistics cargo. With our many years of experience, we manage your shipments, whether they consist of equipment, machinery, or industrial components. Our core shipping services add value to us and become a reason for the success of your business. We tailored your shipping services needs by giving comprehensive solutions for your export and import requirements. `
-      ; 
-    
+      const content = `Arin Tractorwala is your best-trusted partner if you are looking for shipping services in the USA. We take pride in giving you top-notch shipping solutions that meet your export and import needs. With our cutting-edge technology, commitment to excellence, and diverse network of partners, we ensure that your goods reach to right destination efficiently and safely. We provide a wide range of shipping services that meet your needs. Whether you want to export goods, import goods, or want some logistics solutions, then ArinTractorwala is the perfect solution for you.`;
+      const content2 = `Our ocean freight shipping services are designed well to transit a large number of goods across seas. We make strong relationships with shipping lines that allow us to give timely deliveries and competitive rates to our clients. When time is your priority, our air freight services give you an efficient and fast shipping solution. We have a relationship with leading airlines that ensure that your cargo is reached safely to its destination on time. Our team handles all steps of the process, from packaging and customs clearance to timely delivery. We navigate the complexity of your custom regulations and overcome challenges. Our experienced team makes the process simpler, ensures that our compliance is met with customer requirements, and reduces delays. `;
+      const content3 = `We provide you the state of state-of-the-art warehousing facilities and optimize your supply chain. Our shipping services are designed well to meet your demands and ensure that your products reach your destination on time. For heavy or oversize cargo, we are experts in handling project logistics cargo. With our many years of experience, we manage your shipments, whether they consist of equipment, machinery, or industrial components. Our core shipping services add value to us and become a reason for the success of your business. We tailored your shipping services needs by giving comprehensive solutions for your export and import requirements. `;
+      
       return (
         <div>
-          <h1 style={{textAlign: "center"}}>Shipping Page</h1>
-          <ServicesPageContent images={images} content={content} />
+          <CommonBanner title="Shipping"/>
+          {/* <h1 style={{textAlign: "center"}}>Shipping Page</h1> */}
+          <ServicesPageContent images={images} content={content} content2={content2} content3={content3}/>
         </div>
       );
 }
